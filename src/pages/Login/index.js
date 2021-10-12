d527b999262d088acf53cfe029f26ed931e64d42
WendellOliver <wendellofmendes@gmail.com>
2021-10-12 14:48:41 -0300

    2nd commit

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import picture from "../../assets/OmniStack11.png"
 
 export default function Login(){
     return(<div>
         Estou aqui!
+            <img src ={picture} alt=""/>
         <Link to='/register'> lllllllll</Link> 
     </div>);
 }
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import api from "../../services/api";
+//import api from "../../services/api";
+import logo from "../../assets/Logo.png";
+import voltar from "../../assets/Return.png";
+import { Link } from "react-router-dom";
 
 
 export default function Register(){
@@ -13,10 +16,24 @@ export default function Register(){
         e.preventDefault();
         const data={name,email,whatsapp,city,uf};
         console.log(data);
-        const response = await api.post("/ongs", data);
+        //const response = await api.post("/ongs", data);
     }
     return(
-    <form onSubmit={ handleRegister }>
+    <form classname="wrapper"  onSubmit={ handleRegister }>
+        <div classname="all">
+        <img classname="logo" src={logo} alt=""/>
+        <div classname="textarea">
+            <h1 classname="register">Cadastro</h1>
+            <br></br>
+            <h2>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</h2>
+            <br></br>
+            <div classname="returntext">
+                <img src={voltar} alt="" />
+                <Link to='/'>Voltar para o Logon</Link>
+            </div>
+        </div>
+
+        </div>
         <input placeholder="Nome da ONG" type="text" value={name} onChange={e=>setName(e.target.value)}></input>
         <input placeholder="Email" type="text" value={email} onChange={e=>setEmail(e.target.value)}></input>
         <input placeholder="Whatsapp" type="text" value={whatsapp} onChange={e=>setWhatsapp(e.target.value)}></input>
